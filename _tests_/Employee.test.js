71d31195a58471ad5748def7731702d446a4f914
tooqk4u <tooqk4u@protonmail.com>
2022-01-22 22:05:16 -0500

    Employee library and tests completed

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,7 +1,21 @@
 const Employee = require('../lib/Employee.js');
 
-test('creates an employee object', () => {
-  const employee = new Employee;
+test('test all employee properties', () => {
+  const employee = new Employee ("Dan", 123, "tttoo@nnfmd");
 
   expect(typeof(employee)).toBe('object');
+  expect(employee.name).toBe('Dan');
+  expect(employee.id).toBe(123);
+  expect(employee.email).toBe("tttoo@nnfmd");
 })
+
+test('test all employee methods', () => {
+  const employee = new Employee ("Paul", 998, "we@yu")
+
+  expect(employee.getName()).toBe("Paul");
+  expect(employee.getId()).toBe(998);
+  expect(employee.getEmail()).toBe("we@yu");
+  expect(employee.getRole()).toBe("Employee")
+  
+})
+
