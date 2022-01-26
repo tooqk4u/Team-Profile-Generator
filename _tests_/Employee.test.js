4d29a4124303d0cd27301d1b3d41d57cef9ec4c6
tooqk4u <tooqk4u@protonmail.com>
2022-01-25 21:10:36 -0500

    formatted employee test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,21 +1,19 @@
-const Employee = require('../lib/Employee.js');
+const Employee = require("../lib/Employee.js");
 
 test("test all employee properties", () => {
-  const employee = new Employee ("Dan", 123, "tttoo@nnfmd");
+  const employee = new Employee("Dan", 123, "tttoo@nnfmd");
 
   expect(typeof(employee)).toBe("object");
   expect(employee.name).toBe("Dan");
   expect(employee.id).toBe(123);
   expect(employee.email).toBe("tttoo@nnfmd");
-})
+});
 
-test("test all employee methods", () =>{
-  const employee = new Employee ("Paul", 998, "we@yu")
+test("test all employee methods", () => {
+  const employee = new Employee("Paul", 998, "we@yu");
 
   expect(employee.getName()).toBe("Paul");
   expect(employee.getId()).toBe(998);
   expect(employee.getEmail()).toBe("we@yu");
-  expect(employee.getRole()).toBe("Employee")
-  
-})
-
+  expect(employee.getRole()).toBe("Employee");
+});
