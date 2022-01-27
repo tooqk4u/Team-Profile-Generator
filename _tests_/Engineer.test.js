25bb4681c327f4537161dff611f5282ee28da873
tooqk4u <tooqk4u@protonmail.com>
2022-01-26 22:24:20 -0500

    added created HTML functions with fs.writeFile, edited Engineer test, styled anchors

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,19 +1,14 @@
-const Engineer = require('../lib/Engineer.js');
+const Engineer = require("../lib/Engineer.js");
 
 test("all engineer properties", () => {
-  const engineer = new Engineer ("Lisa", 234, "testa@test", "Lisa2112");
+  const engineer = new Engineer("Lisa", 234, "testa@test", "Lisa2112");
 
   expect(engineer.github).toBe("Lisa2112");
-})
+});
 
 test("test all engineer methods", () => {
-  const engineer = new Engineer 
-  ("Lisa", 234, "test@test","Lisa2112");
+  const engineer = new Engineer("Lisa", 234, "test@test", "Lisa2112");
 
   expect(engineer.getGithub()).toBe("Lisa2112");
-  expect(engineer.getRole()).toBe("Engineer")
-})
-
-
-
-
+  expect(engineer.getRole()).toBe("Engineer");
+});
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
-const generateTeam = require("./src/page-template");
+
 
 // team array
 const team = [];
@@ -16,79 +16,117 @@ Answer these questions to generate a new Team-Profile-Page
 `);
 
 // questions
-const questions = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is the team member's name.",
-      validate: (nameInput) => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log("Please enter a name");
-          return false;
-        }
+const employeeQuestions = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name.",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter a name");
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is their employee id? (enter a number)",
-      validate: (idInput) => {
-        const temp = parseInt(idInput);
-        if (temp) {
-          return true;
-        } else {
-          console.log(`Please enter an id number.`);
-          return false;
-        }
+      {
+        type: "input",
+        name: "id",
+        message: "What is their employee id? (enter a number)",
+        validate: (idInput) => {
+          const temp = parseInt(idInput);
+          if (temp) {
+            return true;
+          } else {
+            console.log(`Please enter an id number.`);
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Please enter their email address.",
-      validate: (emailInput) => {
-        if (emailInput) {
-          return true;
-        } else {
-          console.log("Please enter an email.");
-          return false;
-        }
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter their email address.",
+        validate: (emailInput) => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log("Please enter an email.");
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "list",
-      name: "role",
-      message: "Please select team member's role.",
-      choices: ["Engineer", "Intern", "Manager"],
-    },
-  ])
-  .then(function({name, role, id, email}) {
-    let roleInfo = "";
-    if (role === "Engineer") {
-        roleInfo = "GitHub username";
-    } else if (role === "Intern") {
-        roleInfo = "school name";
-    } else {
-        roleInfo = "office number";
-    }
-    inquirer.prompt([{
-        name: "roleInfo",
-        message: `Enter team member's ${roleInfo}`,
-    },
-    {
+      {
         type: "list",
-        name: "addMembers",
-        message: "Would you like to add more team members?",
-        choices: [
-            "yes",
-            "no"
-        ],
-    }])
-  })
+        name: "role",
+        message: "Please select employee's role.",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+    ])
+    .then(function ({ name, role, id, email }) {
+      let roleInfo = "";
+      if (role === "Manager") {
+        roleInfo = "office number";
+      } else if (role === "Engineer") {
+        roleInfo = "GitHub username";
+      } else {
+        roleInfo = "school name";
+      }
+      inquirer
+        .prompt([
+          {
+            name: "roleInfo",
+            message: `Enter employee's ${roleInfo}`,
+          },
+          {
+            type: "list",
+            name: "addMembers",
+            message: "Would you like to add more employee's?",
+            choices: ["yes", "no"],
+          },
+        ])   
+        
+    });
 };
+ //create HTML
+ function createHTML() {
+   const html =
+   `<!DOCTYPE html>
+   <html lang="en">
+     <head>
+       <meta charset="UTF-8" />
+       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+       <meta name="viewport" content="width=device-width, 
+   initial-scale=1.0" />
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/
+   dist/css/bootstrap.min.css" rel="stylesheet" 
+   integrity="sha384-
+   +0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7
+   +AMvyTG2x" crossorigin="anonymous">
+       <link rel="stylesheet" href="https://use.fontawesome.com/
+   releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ
+   +1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" 
+   crossorigin="anonymous"/>
+       <link rel="stylesheet" href="./dist/style.css">
+     
+       <title>Team Profile</title>
+     </head>
+     <body class="bg-light">  
+       <header>
+         <h1 class="bg-danger bg-gradient text-center text-light 
+   p-3">Team Profile</h1>
+     </header>   
+     <main>`;
 
-
-questions();
+     fs.writeFile("./dist/output-HTML", html, function(err) {
+      if (err) {
+          console.log(err);
+         } 
+     }
+    )
+}; 
+employeeQuestions();
+createHTML();
