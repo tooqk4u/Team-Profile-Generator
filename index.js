52c1d6af4b18b3242f0bd44ff837d4d6bbaa58cb
tooqk4u <tooqk4u@protonmail.com>
2022-01-29 22:42:43 -0500

    added team title function

--- index.js
+++ index.js
@@ -5,20 +5,44 @@ const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 
+//team name 
 // team array
-const team = [];
+let team = [];
 
-function initialize() {
-  createHTML();
-  employeeQuestions();
+async function initialize() {
+    await nameTeam();
+    
+  
 }
 
 console.log(`
-==========================================================
-Answer these questions to generate a new Team-Profile-Page
-==========================================================
+=========================================================+
+|                                                       ||
+|Answer these questions to generate a Team-Profile-Page ||
+|                                                       ||
+=========================================================
 `);
 
+
+const nameTeam = () => {
+  inquirer.prompt([
+      {   type: "input",
+          name: "teamName",
+          message: "Welcome to Team Profile Generator! Please enter your team name:",
+          
+      }
+    ])
+          .then(function(data) {
+           let teamName = data.teamName;
+           team.push(teamName);
+           employeeQuestions ();
+           createHTML();  
+    
+  }) 
+}
+      
+
+    
 // questions
 const employeeQuestions = () => {
   inquirer
@@ -31,8 +55,7 @@ const employeeQuestions = () => {
           if (nameInput) {
             return true;
           } else {
-            console.log("Please enter a name");
-            return false;
+            return "Please enter a name";
           }
         },
       },
@@ -41,12 +64,14 @@ const employeeQuestions = () => {
         name: "id",
         message: "What is their employee id? (enter a number)",
         validate: (idInput) => {
-          const temp = parseInt(idInput);
-          if (temp) {
+          const valid =  /^[0-9]+$/.test(idInput);
+          if (valid) {
             return true;
-          } else {
-            console.log(`Please enter an id number.`);
-            return false;
+          } else { 
+            console.log("Pleae enter a number.")            
+            return ;
+            
+            
           }
         },
       },
@@ -54,12 +79,14 @@ const employeeQuestions = () => {
         type: "input",
         name: "email",
         message: "Please enter their email address.",
-        validate: (emailInput) => {
-          if (emailInput) {
+        validate: (email) => {
+          const valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+          if (valid) {
             return true;
           } else {
             console.log("Please enter an email.");
             return false;
+            
           }
         },
       },
@@ -113,9 +140,12 @@ const employeeQuestions = () => {
       });
 };
 
+
+
 // add create HTML function
 function createHTML() {
-  const html = `<!DOCTYPE html>
+  
+   const html = `<!DOCTYPE html>
    <html lang="en">
      <head>
        <meta charset="UTF-8" />
@@ -124,11 +154,11 @@ function createHTML() {
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>
        <link rel="stylesheet" href="./dist/style.css">
-       <title>Team Profile</title>
+       <title> Team Profile Generator | ${team[0]} </title>
      </head>
      <body class="bg-light">  
        <header>
-         <h1 class="bg-danger bg-gradient text-center text-light p-3">Team Profile</h1>
+         <h1 class="bg-danger bg-gradient text-center text-light p-3">${team[0]}</h1>
        </header>`;
 
   fs.writeFile("./dist/index.html", html, function (err) {
@@ -185,7 +215,7 @@ function addEmployeeHTML(employee) {
   <div class="card rounded-3 bg-secondary m-2" style="width: 300px" >
    <div class="card-title bg-primary text-light p-2">
      <h3>${name}</h3>
-     <h5><i class="fas fa-school"></i> ${role}</h5>
+     <h5><i class="fas fa-user-graduate"></i> ${role}</h5>
    </div>
   <div class="card-body ">
     <div class="list-group list-group-flush">
@@ -208,10 +238,9 @@ function addEmployeeHTML(employee) {
 
 function finishHTML() {
   const html = ` 
-    <main>
-      <div>
-      </div>
-    </main>
+      <footer class="bg-danger bg-gradient text-center text-light p-3 fixed-bottom">
+      <div>&copy;tooqk4u 2022 <bdo dir="rtl">&copy;</bdo></div>
+    </footer>
   </body>
   </html>`;
 
@@ -220,27 +249,11 @@ function finishHTML() {
       console.log(err);
     } else {
       console.log(`
-      ==================================================================================
+      ===============================================================================
       "Team Profile complete! Check index.html in dist directory to see the output!"
-      ==================================================================================
+      ===============================================================================
       `);
     }
   });
 }
 initialize();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
